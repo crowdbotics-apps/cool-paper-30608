a88b09cf039e01c90f9e5166489a92b508f2580b
Crowdbotics <team@crowdbotics.com>
2021-09-16 17:01:27 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "cool_paper_30608Identifier",
+  url: "https://cool_paper_30608Identifier.botics.co",
+  api: "https://cool_paper_30608Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
